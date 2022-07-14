d86fb9c6251389f7a398b34802e1eb9510f8cb69
valdidev <valdicode.fernando@gmail.com>
2022-07-14 10:51:51 +0200

    main part done

--- app.js
+++ app.js
@@ -4,13 +4,30 @@ let weather = {
         fetch(
             `https://api.openweathermap.org/data/2.5/weather?q=${place}&units=metric&appid=${this.apiKey}`
             ).then(res => res.json())
-             .then(data => this.displayWeather(data))
+             .then(data => this.displayWeather(data));
     },
     displayWeather: function (data) {
         const { name } = data;
         const { icon, description } = data.weather[0]
         const { temp, humidity } = data.main;
         const { speed } = data.wind;
-        console.log(name, icon, description, temp, humidity, speed)
+        document.getElementById("city").textContent = name;
+        document.getElementById("icon").setAttribute("src", `https://openweathermap.org/img/wn/${icon}.png`)
+        document.getElementById("description").textContent = description;
+        document.getElementById("humidity").textContent = `Humidity: ${humidity}%`;
+        document.getElementById("wind").textContent = `Wind Speed: ${speed}km/h`;
+        document.getElementById("weather").classList.remove("loading");
+    },
+    search: function () {
+        this.fetchWeather(document.getElementById("searchBar").value)
     }
-}
+};
+
+document.getElementById("searchButton").addEventListener("click", () => weather.search());
+document.getElementById("searchBar").addEventListener("keyup", e => {
+    if (e.key == "Enter"){
+        weather.search();
+    }
+});
+
+weather.fetchWeather("valencia");
